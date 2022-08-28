89e3eeddf93a74c55408449422b9f7cb1eb1f903
yamamo0301 <yamamo0301@icloud.com>
2022-08-28 14:52:37 +0000

    [add][layouts]score_sheet_show_page

--- app/models/score_sheet.rb
+++ app/models/score_sheet.rb
@@ -4,4 +4,21 @@ class ScoreSheet < ApplicationRecord
   has_many :sheets, dependent: :destroy
   has_many :scores, dependent: :destroy
 
+
+  def table_point_calculation(player)
+    if rule.calculation_status == "subtraction_evenness"
+      "TP#{scores.where(player_id: player).size * -(rule.table_point)}P"
+    elsif rule.calculation_status == "subtraction_top"
+      "TP#{scores.where(player_id: player, rank: 1).size * -(rule.table_point)}P"
+    elsif score_sheet.rule.calculation_status == "subtraction_bottom"
+      "TP#{scores.where(player_id: player, rank: 4).size * -(rule.table_point)}P"
+    else
+      "TP0P"
+    end
+  end
+
+  def total_point(player)
+    scores.where(player_id: player).sum(:tip) * rule.tip_point + scores.where(player_id: player).sum(:point)
+  end
+
 end
