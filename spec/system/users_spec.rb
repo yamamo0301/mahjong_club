6722aa9c9830cd9a06731b337e8d6338601ef94d
yamamo0301 <yamamo0301@icloud.com>
2022-10-25 13:31:05 +0000

    [fix]public/users/show.html.erb

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,13 +6,9 @@ describe 'ログインとログアウト' do
   end
   it "login" do
     visit new_user_session_path
-
-  #   # # ログインフォームにEmailとパスワードを入力する
     fill_in 'user_email', with: @user.email
     fill_in 'パスワード', with: @user.password
-  #   # # ログインボタンをクリックする
     click_button 'ログイン'
-  #   # # ログインに成功したことを検証する
     expect(page).to have_content 'ログインしました'
   end
 end
